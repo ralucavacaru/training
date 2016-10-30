ba746c1123c833c2df75274d16ba1d324312633b
ralucavacaru <raluca_vacaru@yahoo.com>
2016-10-30 21:11:24 +0000

    Added nested states for Browse

--- js/training.js
+++ js/training.js
@@ -8,25 +8,55 @@ Training.config(function($stateProvider, $urlRouterProvider) {
     url: '/',
     templateUrl: 'partials/home.html'
   }
-
   var browseState = {
     name: 'browse',
     url: '/browse',
-    templateUrl: 'partials/browse.html'
+    templateUrl: 'partials/browse.html',
+    redirectTo: 'browse.all'
   }
-
+  var allState = {
+    name: 'browse.all',
+    url: '',
+    templateUrl: 'partials/all.html'
+  }
+  var strategyState = {
+    name: 'browse.strategy',
+    url: '/strategy',
+    templateUrl: 'partials/strategy.html'
+  }
+  var contentState = {
+    name: 'browse.content',
+    url: '/content',
+    templateUrl: 'partials/content.html'
+  }
+  var miscState = {
+    name: 'browse.misc',
+    url: '/misc',
+    templateUrl: 'partials/misc.html'
+  }
   var aboutState = {
     name: 'about',
     url: '/about',
     templateUrl: 'partials/about.html'
   }
 
-  $stateProvider.state(homeState);
-  $stateProvider.state(browseState);
-  $stateProvider.state(aboutState);
+  $stateProvider
+        .state(homeState)
+        .state(browseState)
+        .state(allState)
+        .state(strategyState)
+        .state(contentState)
+        .state(miscState)
+        .state(aboutState);
 });
 
 Training.controller('HomeCtrl', function($scope, MockDataFactory) {
+});
+
+Training.controller('BrowseCtrl', function($scope, MockDataFactory) {
+});
+
+Training.controller('AllCtrl', function($scope, MockDataFactory) {
     $scope.trainings = MockDataFactory.trainings;
 });
 
