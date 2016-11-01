ab9e44c58f9fef45cfec5c425341e600237adc8b
ralucavacaru <raluca_vacaru@yahoo.com>
2016-11-01 21:38:31 +0000

    Browse bar & scroll-then-fixed effect

--- js/training.js
+++ js/training.js
@@ -50,6 +50,10 @@ Training.config(function($stateProvider, $urlRouterProvider) {
         .state(miscState)
         .state(aboutState);
 });
+Training.controller('IndexCtrl', function($scope, MockDataFactory) {
+  $scope.scrollTo = function (target){
+  };
+});
 
 Training.controller('HomeCtrl', function($scope, MockDataFactory) {
 });
@@ -236,3 +240,23 @@ Training.run(['$rootScope', '$state', function($rootScope, $state) {
       }
     });
 }]);
+
+Training.directive('setClassWhenAtTop', function ($window) {
+    var $win = angular.element($window); // wrap window object as jQuery object
+
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var topClass = attrs.setClassWhenAtTop, // get CSS class from directive's attribute value
+                offsetTop = element.offset().top; // get element's top relative to the document
+
+            $win.on('scroll', function (e) {
+                if ($win.scrollTop() >= offsetTop) {
+                    element.addClass(topClass);
+                } else {
+                    element.removeClass(topClass);
+                }
+            });
+        }
+    };
+})
